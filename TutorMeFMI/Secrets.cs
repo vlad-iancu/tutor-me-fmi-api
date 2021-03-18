5981cb958c681b0bf1e09c27846f381e1ba78729
vlad-iancu <iancuvladalexandru@gmail.com>
2021-03-19 01:30:33 +0200

    Initial Commit

--- TutorMeFMI/Secrets.cs
+++
@@ -1,8 +0,0 @@
-namespace TutorMeFMI
-{
-    public static class Secrets
-    {
-        public const string ConnectionString = "Host=localhost;Port=3306;User=tutor_me_fmi_user;Password=mysql;Database=tutor_me_fmi;SslMode=None";
-        public const string JwtSecret = "thisismysupersecretjwtsecret";
-    }
-}
