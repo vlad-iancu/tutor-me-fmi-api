df64891e617acea4f9bf388c7d64f5e54bea6459
vlad-iancu <iancuvladalexandru@gmail.com>
2021-04-08 19:03:00 +0300

    Updated Schema

--- TutorMeFMI/Data/schema.sql
+++ TutorMeFMI/Data/schema.sql
@@ -1,20 +1,29 @@
+DROP TABLE IF EXISTS offer;
 DROP TABLE IF EXISTS request;
 DROP TABLE IF EXISTS user;
-CREATE TABLE user
-(
-    id       INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    email    VARCHAR(128) UNIQUE,
-    password VARCHAR(128),
-    name     VARCHAR(128)
+CREATE TABLE user(
+                     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                     email VARCHAR(128) UNIQUE,
+                     password VARCHAR(128),
+                     name VARCHAR(128)
 );
-CREATE TABLE request
-(
-    id           INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    title        VARCHAR(128),
-    description  VARCHAR(1024),
-    price        INT,
-    meetingType  VARCHAR(128),
-    meetingSpecs VARCHAR(128),
-    userId       INT,
-    CONSTRAINT FK_REQUEST_USER FOREIGN KEY (userId) REFERENCES user (id)
+CREATE TABLE request(
+                        id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                        title VARCHAR(128),
+                        description VARCHAR(1024),
+                        price INT,
+                        meetingType VARCHAR(128),
+                        meetingSpecifications VARCHAR(128),
+                        userId INT,
+                        CONSTRAINT FK_REQUEST_USER FOREIGN KEY (userId) REFERENCES user(id)
+);
+CREATE TABLE offer(
+                      id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                      title VARCHAR(128),
+                      description VARCHAR(1024),
+                      price INT,
+                      meetingType VARCHAR(128),
+                      meetingSpecifications VARCHAR(128),
+                      userId INT,
+                      CONSTRAINT FK_OFFER_USER FOREIGN KEY (userId) REFERENCES user(id)
 );
