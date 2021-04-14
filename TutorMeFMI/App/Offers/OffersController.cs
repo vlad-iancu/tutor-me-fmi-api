8a1e0b925264137f234c4c5f042f8d233e5de55d
Bianca Caldaruse <B.Caldaruse@student.tudelft.nl>
2021-04-14 12:31:03 +0300

    Revert "Add methods to retrieve global offers and requests, update comments for these methods"
    This reverts commit 3f20c1012d53a041d5c3acbea4828109e53df7c4.

--- TutorMeFMI/App/Offers/OffersController.cs
+++ TutorMeFMI/App/Offers/OffersController.cs
@@ -1,7 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using SqlKata.Execution;
-using TutorMeFMI.App.Auth;
-using TutorMeFMI.App.Auth.Model;
 using TutorMeFMI.Data;
 using TutorMeFMI.Models;
 
@@ -11,32 +10,14 @@ namespace TutorMeFMI.Controllers
     {
         /**
          * GET method that returns all the existing offers posted by an user
-         * param @user = User entity representing the user to retrieve the offers for
+         * param @userId = int value representing the id of the user to retrieve the offers for
          * Returns a list of type Offer containing the retrieved offers
          */
-        [HttpGet]
-        [Authorization]
-        public IActionResult UserOfs(User user)
+        public IActionResult UserOfs(int userId)
         {
             using var database = new Database().GetQueryFactory();
-            var offers = database.Query("offer").Where("email", "=", user.Email).Get<Offer>();
-            return Json(new {offers});
-        }
-        
-        
-        /**
-        * GET method that returns all the existing offers posted by all the other users
-        * other than @user itself
-        * param @user = User entity representing the user to retrieve the global offers for
-        * Returns a list of type Offer containing the retrieved offers
-        */
-        [HttpGet]
-        [Authorization]
-        public IActionResult AllOfs(User user)
-        {
-            using var database = new Database().GetQueryFactory();
-            var offers = database.Query("offer").Where("email", "!=", user.Email).Get<Offer>();
-            return Json(new {offers});
+            var offers = database.Query("offer").Where("user_id", "=", userId).Get<Offer>();
+            return View();
         }
     }
 }
--- TutorMeFMI/App/Requests/RequestsController.cs
+++ TutorMeFMI/App/Requests/RequestsController.cs
@@ -11,30 +11,15 @@ namespace TutorMeFMI.App.Requests
     {
         /**
          * GET method that returns all the existing requests posted by an user
-         * param @user = User entity representing the user to retrieve the requests for
+         * param @userId = int value representing the id of the user to retrieve the requests for
          * Returns a list of type Request containing the retrieved requests
          */
         [HttpGet]
         [Authorization]
-        public IActionResult UserReqs(User user)
+        public IActionResult List(User user)
         {
             using var database = new Database().GetQueryFactory();
             var requests = database.Query("request").Where("email", "=", user.Email).Get<Request>();
-            return Json(new {requests});
-        }
-        
-        /**
-         * GET method that returns all the existing requests posted by all the other users
-         * other than @user itself
-         * param @user = User entity representing the user to retrieve the global requests for
-         * Returns a list of type Request containing the retrieved requests
-         */
-        [HttpGet]
-        [Authorization]
-        public IActionResult AllReqs(User user)
-        {
-            using var database = new Database().GetQueryFactory();
-            var requests = database.Query("request").Where("email", "!=", user.Email).Get<Request>();
             return Json(new {requests});
         }
     }
